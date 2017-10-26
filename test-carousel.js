68e5f6dd20f3aa1364bb69b7a3257cb8733bdb1e
mari <gamuga288@gmail.com>
2017-10-26 08:05:29 +0000

    git commit1

---
+++ test-carousel.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel(
+        {
+            loop: true,
+            nav: false,
+            dots:  true,
+            items: 1,
+            autoplay: true
+        }
+    );
+});
+
+
+
+$(document).ready(function(){
+  $("#menu").click(function(){
+    $(".sp_menu_toggle").slideToggle();
+  });
+});
